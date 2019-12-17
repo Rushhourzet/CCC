1f8d3a7c0d5afeab2a84629ad269aaa99215f9fd
Rushhourz <sascha.hampl@outlook.com>
2019-12-17 16:58:22 +0100

    Worked some at Harvester

--- Harvester/src/at/Harvester/Field.java
+++ Harvester/src/at/Harvester/Field.java
@@ -1,26 +1,28 @@
 package at.Harvester;
 
 public class Field {
+//_______________________________Fields_______________________________
     private int rows, columms;
-
+//________________________________Constructor___________________________
     public Field(int rows, int columms) {
         this.rows = rows;
         this.columms = columms;
     }
-
+//_________________________________Getter_______________________________
     public int getRows() {
         return rows;
     }
-
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumms() {
         return columms;
     }
 
+//__________________________________Setter_______________________________
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
     public void setColumms(int columms) {
         this.columms = columms;
     }
+//_________________________________Methods_1_____________________________
+
 }
--- Harvester/src/at/Harvester/Harvest.java
+++ Harvester/src/at/Harvester/Harvest.java
@@ -1,16 +1,20 @@
 package at.Harvester;
 
+import java.awt.*;
+
 public class Harvest {
-//_____________________________________Fields________________________________________________
+//_____________________________________Data_Fields________________________________________________
     private Field f;
     private char direction;
     private int startCol, startRow;
+    private Point startPoint, leftTopCorner, rightTopCorner, leftBottomCorner, rightBottomCorner;
 //_____________________________________Constructor___________________________________________
     public Harvest(int columns, int rows,int startCol, int startRow, char direction) {
         setF(columns, rows);
         setStartCol(startCol);
         setStartRow(startRow);
         setDirection(direction);
+
     }
 //_____________________________________Setter____________________________________________
     public void setF(int columns, int rows) {
@@ -28,6 +32,10 @@ public class Harvest {
     public void setDirection(char direction) {
         this.direction = direction;
     }
+
+
+
+
 //___________________________________________Getter_______________________________________
     public int getStartCol() {
         return startCol;
@@ -43,4 +51,50 @@ public class Harvest {
     }
 //_________________________________________Methods_1__________________________________________
 
+    public void calculatePath(){
+        int tmp = startPoint.x*startPoint.y;
+       if(direction == 'O'){
+
+       }
+
+    }
+//________________________________________Methods_2_(secondary Methods)____________________________________________
+
+    public void generatePoints(){
+        generateStartPoint();
+        generateLeftTopCorner();
+        generateLeftBottomCorner();
+        generateRightTopCorner();
+        generateRightBottomCorner();
+    }
+//_______________________________________Methods_3_(tertiary Methods)___________________________________________________
+    //___move____
+    public int moveEast(int input){
+        return input+1;
+    }
+    public int moveWest(int input){
+        return input-1;
+    }
+    public int moveSouth(int input){
+        return input+f.getColumms();
+    }
+    public int moveNorth(int input){
+        return input-f.getColumms();
+    }
+    //__generatePoints__
+    public void generateStartPoint() {
+        this.startPoint = new Point(startCol, startRow);
+    }
+    public void generateLeftTopCorner() {
+        this.leftTopCorner = new Point(1,1);
+    }
+    public void generateRightTopCorner() {
+        this.rightTopCorner = new Point(f.getColumms(), 1);
+    }
+    public void generateLeftBottomCorner() {
+        this.leftBottomCorner = new Point(1, f.getRows());
+    }
+    public void generateRightBottomCorner() {
+        this.rightBottomCorner = new Point(f.getColumms(), f.getRows());
+    }
 }
