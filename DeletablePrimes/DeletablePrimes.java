b99cff2f9a4eb56d1e157f115b88b89896e9d437
Rushhourz <sascha.hampl@outlook.com>
2020-03-21 12:07:37 +0100

    DeleteablePrimes without Recursion(unfinished)

---
+++ DeletablePrimes/DeletablePrimes.java
@@ -0,0 +1,48 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class DeletablePrimes{
+    private static int CERTAINTY = 10;
+    BigInteger[][] nums;
+    String numString;
+    int counter;
+
+    public DeletablePrimes(BigInteger num){
+        counter = 0;
+        numString = num.toString();
+        nums = new BigInteger[numString.length()][];
+        nums[0] = new BigInteger[]{num};
+    }
+    public DeletablePrimes(String numAsString){
+        BigInteger num = new BigInteger(numAsString);
+        counter = 0;
+        numString = num.toString();
+        nums = new BigInteger[numString.length()][];
+        nums[0] = new BigInteger[]{num};
+    }
+
+    public void Run(){
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = Solve(nums[i-1]);
+        }
+        System.out.println(counter);
+    }
+
+    public static boolean CheckIfPrime(BigInteger num){
+        return num.isProbablePrime(CERTAINTY);
+    }
+
+    private BigInteger[] Solve(BigInteger[] nums){
+        ArrayList<Object> tmp = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            String tmpString = nums[i].toString();
+            StringBuilder builder = new StringBuilder(tmpString);
+            for (int j = 0; j < tmpString.length(); j++) {
+                BigInteger tmpBI = new BigInteger(builder.deleteCharAt(j).toString());
+                if(CheckIfPrime(tmpBI)) tmp.add(tmpBI); 
+                if(tmpBI.intValue()<10) this.counter++;
+            }
+        }
+        return (BigInteger[])tmp.toArray();
+    }
+}
---
+++ DeletablePrimes/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args) {
+        DeletablePrimes d1 = new DeletablePrimes("4567");
+        d1.Run();
+    }
+}
